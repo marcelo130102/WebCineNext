2527b0ede45d483794d0660a2acb449869aee436
neftalics <neftali.calixto@utec.edu.pe>
2022-06-16 23:39:48 -0500

    t+2

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import '../styles/Footer.module.css'
 export function Footer(){
     return(
       <div style={{backgroundColor:"black",color:"white", textAlign: 'center', height:100}}>
-      <footer style={{paddingTop:20}}>Trinidad</footer>
+      <footer style={{paddingTop:20}}>Trinidad+2</footer>
            <footer>2022-2022</footer>
            <footer>All rights Reserved.</footer>
       </div>
