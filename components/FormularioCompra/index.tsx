cad058d11a4b8170c7e7a444c10c603a899fa007
Marcelo Surco <marcelo.surco@utec.edu.pe>
2022-06-20 23:55:18 -0500

    Terminando fomrulario y agregando comidas

--- components/FormularioCompra/index.tsx
+++ components/FormularioCompra/index.tsx
@@ -6,11 +6,11 @@ export default function Formulario(props:any) {
   const idPelicula = props.idPelicula;
   return (
     <div style={{ background: "#2A0B45", padding:"50px", color:"white" }}>
-      <Grid grow>
+      <Grid align="center" grow>
         <Grid.Col md={6} lg={4} style={{justifyContent:"center", alignItems:"center"}}>
           <Group position="center">
+          <Title style={{textAlign:"center"}} order={1}>Ver: {props.title}</Title>
           <Image width={400} src={imgURL} alt={props.title} />
-          <Title style={{textAlign:"center"}} order={1}>{props.title}</Title>
           </Group>
         </Grid.Col>
         <Grid.Col md={8} lg={7}>
